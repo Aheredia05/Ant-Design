d784496ae598115cec11b2974f5cb61f10ac9103
Alfonso Heredia <aheredia.estudio@gmail.com>
2020-01-21 12:38:40 -0500

    first commit2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from 'ant/es/button';
+import { Input } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+
+  );
+}
+
+export default App;
